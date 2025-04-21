572d30ed14983e64a4bc5fecded163d291f26414
garnellos <114871622+garnellos@users.noreply.github.com>
2025-04-21 23:40:01 +0200

    added class TaskList, added some hierarchy logic

--- task.js
+++ task.js
@@ -11,6 +11,9 @@ class Task
     /** parent task for hierarchy */
     parent;
 
+    /** child tasks for hierarchy */
+    #children;
+
     /** time of creation of task */
     #creationDate;
 
@@ -23,11 +26,24 @@ class Task
 
     // constructor
 
+    /**
+     * Constructs a new task object with all necessary attributes.
+     * @param {string} title title of the task as to be displayed in task list
+     * @param {string} description full-sized details on this task
+     * @param {Task} parent parent task, this task will automatically be added to its parents child list. for a top-level task, pass null.
+     * @param {Date} creationDate time of creation of task
+     * @param {Date} dueDate time when task is due
+     * @param {boolean} done determines if task is done
+     */
     constructor(title, description, parent, creationDate, dueDate, done)
     {
         this.title = title;
         this.description = description;
+
         this.parent = parent;
+        if (parent != null) parent.addChild(this); // add to parents' list if no top-level task
+        
+        this.children = [];
         this.#creationDate = creationDate;
         this.dueDate = dueDate;
         this.done = done;
@@ -36,17 +52,37 @@ class Task
 
     // getters
 
+    /** @returns creation date of the task. */
     get creationDate()
     {
         return this.#creationDate;
     }
 
+    /** @returns array of all child tasks. */
+    get children()
+    {
+        return this.#children;
+    }
 
     // methods
     
+    /** Adds a child task. */
+    addChild(t)
+    {
+        this.#children.push(t);
+    }
+
     /** Marks a task as done. */
     done()
     {
         done = true;
     }
+}
+
+class TaskList
+{
+    // fields
+
+    /** array of top-level tasks */
+    #tasks;
 }
