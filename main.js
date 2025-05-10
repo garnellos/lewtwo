e691f575f7ac612110a6d3f40fc91a93d67a1f14
Philipp <philipp@terstappen.eu>
2025-05-10 10:41:48 +0200

    Added access to modules from window object to allow debug from the console

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
+import { Task } from "./model/Task.js";
 import { TaskList } from "./model/TaskList.js";
 import { TaskListPanel } from "./ui/TaskListPanel.js";
+import { TaskDetailPanel } from "./ui/TaskDetailPanel.js";
+import { TaskDate } from "./util/TaskDate.js";
+
+// debug assignments so modules can be used in the console
+window.logic = {
+    model: {},
+    util: {},
+    ui: {}
+}
+window.logic.model.Task = Task;
+window.logic.model.TaskList = TaskList;
+window.logic.util.TaskDate = TaskDate;
+window.logic.ui.TaskListPanel = TaskListPanel;
+window.logic.ui.TaskDetailPanel = TaskDetailPanel;
+// end debug assignments
 
 let taskList = new TaskList();
 TaskListPanel.render(taskList);
